179ca76a8233f702a89c947397483c45d174541a
Artem <sherhaninho1337@gmail.com>
2021-07-07 02:49:00 +0300

    teper' krasivo

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-let joinbuts = document.querySelectorAll('.all_buttons_join');
-for(let joinbut of joinbuts) {
-    joinbut.onclick = function (event) {
-        window.location.href = './join.html'
-    }
-    
+let joinbuts = document.querySelectorAll(".all_buttons_join");
+for (let joinbut of joinbuts) {
+  joinbut.onclick = function (event) {
+    window.location.href = "./join.html";
+  };
 }
--- js/modal.js
+++ js/modal.js
@@ -1,42 +1,40 @@
-let start = document.querySelector('.start')
-let modal = document.querySelector('.modal__window')
-let filter = document.querySelector('.filter')
-let inputSS = document.querySelectorAll('.InputStyle')
-let submitButton = document.querySelector('.submit__button')
-let invalidInput = document.querySelector('.incorrect')
+let start = document.querySelector(".start");
+let modal = document.querySelector(".modal__window");
+let filter = document.querySelector(".filter");
+let inputSS = document.querySelectorAll(".InputStyle");
+let submitButton = document.querySelector(".submit__button");
+let invalidInput = document.querySelector(".incorrect");
 
-
-start.onclick = function(visible) {
-    modal.style.visibility = 'visible';
-    filter.style.visibility = 'visible'
-}
-filter.onclick = function(hidden) {
-    modal.style.visibility = 'hidden';
-    filter.style.visibility = 'hidden'
-    invalidInput.style.visibility = 'hidden'
-}  
+start.onclick = function (visible) {
+  modal.style.visibility = "visible";
+  filter.style.visibility = "visible";
+};
+filter.onclick = function (hidden) {
+  modal.style.visibility = "hidden";
+  filter.style.visibility = "hidden";
+  invalidInput.style.visibility = "hidden";
+};
 for (let inputS of inputSS) {
-    inputS.oninput = function(checkInput) {
-        if (inputS.value.length <= 10) {
-            inputS.style.borderColor = "red";
-            submitButton.disabled = true
-        }else {
-            inputS.style.borderColor = "green";
-            submitButton.disabled = false
-        }
+  inputS.oninput = function (checkInput) {
+    if (inputS.value.length <= 10) {
+      inputS.style.borderColor = "red";
+      submitButton.disabled = true;
+    } else {
+      inputS.style.borderColor = "green";
+      submitButton.disabled = false;
     }
+  };
 }
 for (let inputS of inputSS) {
-    inputS.onchange = function(checkChange) {
-        if (inputS.value.length <= 10) {
-            inputS.style.borderColor = "red";
-            submitButton.disabled = true;
-            invalidInput.style.visibility = 'visible'
-        }else {
-            inputS.style.borderColor = "green";
-            submitButton.disabled = false;
-            invalidInput.style.visibility = 'hidden'
-        }
+  inputS.onchange = function (checkChange) {
+    if (inputS.value.length <= 10) {
+      inputS.style.borderColor = "red";
+      submitButton.disabled = true;
+      invalidInput.style.visibility = "visible";
+    } else {
+      inputS.style.borderColor = "green";
+      submitButton.disabled = false;
+      invalidInput.style.visibility = "hidden";
     }
+  };
 }
-
